d63ba38c2d224ee63599e0a7e9cef53d67e3432b
Franklin Callejas <203692046+FICT1@users.noreply.github.com>
2026-09-16 15:43:36 -0600

    feat: Model y Util creados

---
+++ src/main/java/ni/edu/uam/eventosnavegacionypasodatos/model/Cliente.java
@@ -0,0 +1,4 @@
+package ni.edu.uam.eventosnavegacionypasodatos.model;
+
+public class Cliente {
+}
---
+++ src/main/java/ni/edu/uam/eventosnavegacionypasodatos/util/SceneManager.java
@@ -0,0 +1,4 @@
+package ni.edu.uam.eventosnavegacionypasodatos.util;
+
+public class SceneManager {
+}
