f144f43059ae1a5d7840af7bb7491c3dc594741a
Daris Mayorga <ddmayorga@uamv.edu.ni>
2026-09-16 18:32:00 -0600

    feat:Mostrar Alerta

--- src/main/java/ni/edu/uam/eventosnavegacionypasodatos/controllers/RegistroClienteController.java
+++ src/main/java/ni/edu/uam/eventosnavegacionypasodatos/controllers/RegistroClienteController.java
@@ -101,18 +101,16 @@ public class RegistroClienteController {
         imgFotografia.setImage(null);
     }
 
-}
+    private void cerrarVentana() {
+        Stage stage = (Stage) btnCancelar.getScene().getWindow();
+        stage.close();
+    }
 
-private void cerrarVentana() {
-    Stage stage = (Stage) btnCancelar.getScene().getWindow();
-    stage.close();
-}
-
-private void mostrarAlerta(Alert.AlertType tipo, String titulo, String mensaje) {
-    Alert alert = new Alert(tipo);
-    alert.setTitle(titulo);
-    alert.setHeaderText(null);
-    alert.setContentText(mensaje);
-    alert.showAndWait();
-}
+    private void mostrarAlerta(Alert.AlertType tipo, String titulo, String mensaje) {
+        Alert alert = new Alert(tipo);
+        alert.setTitle(titulo);
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
+    }
 }
