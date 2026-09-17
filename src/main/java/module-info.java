ea7a19977dd3265ef9a745ade9618bee7b78fc2d
Daris Mayorga <ddmayorga@uamv.edu.ni>
2026-09-16 18:29:53 -0600

    feat:guardar nombre del cliente

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module ni.edu.uam.eventosnavegacionypasodatos {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens ni.edu.uam.eventosnavegacionypasodatos to javafx.fxml;
