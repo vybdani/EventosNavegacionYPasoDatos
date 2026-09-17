8ea17f9d279c1386986cf011c43a5a081b67d204
Daniella Alexandra Rocha Palacios <darochap@uamv.edu.ni>
2026-09-16 22:18:12 -0600

    feat: ampliar modelo Cliente con campos de tipo, ciudad, fecha y solicitud

--- src/main/java/ni/edu/uam/eventosnavegacionypasodatos/model/Cliente.java
+++ src/main/java/ni/edu/uam/eventosnavegacionypasodatos/model/Cliente.java
@@ -3,6 +3,8 @@ package ni.edu.uam.eventosnavegacionypasodatos.model;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+import java.time.LocalDate;
+
 public class Cliente {
 
     public static final ObservableList<Cliente> LISTA_CLIENTES = FXCollections.observableArrayList();
@@ -12,6 +14,14 @@ public class Cliente {
     private String correo;
     private String telefono;
 
+    private String tipoCliente;
+    private String ciudad;
+    private LocalDate fechaNacimiento;
+    private String tipoSolicitud;
+    private boolean mantenimiento;
+    private boolean instalacion;
+    private String fotoPath;
+
     public Cliente() {
     }
 
@@ -20,6 +30,20 @@ public class Cliente {
         this.apellido = apellido;
         this.correo = correo;
         this.telefono = telefono;
+    }
+
+    public Cliente(String nombre, String apellido, String tipoCliente, String ciudad,
+                   LocalDate fechaNacimiento, String tipoSolicitud,
+                   boolean mantenimiento, boolean instalacion, String fotoPath) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.tipoCliente = tipoCliente;
+        this.ciudad = ciudad;
+        this.fechaNacimiento = fechaNacimiento;
+        this.tipoSolicitud = tipoSolicitud;
+        this.mantenimiento = mantenimiento;
+        this.instalacion = instalacion;
+        this.fotoPath = fotoPath;
     }
 
     public String getNombre() {
@@ -38,6 +62,10 @@ public class Cliente {
         this.apellido = apellido;
     }
 
+    public String getNombreCompleto() {
+        return nombre + " " + apellido;
+    }
+
     public String getCorreo() {
         return correo;
     }
@@ -52,5 +80,61 @@ public class Cliente {
 
     public void setTelefono(String telefono) {
         this.telefono = telefono;
+    }
+
+    public String getTipoCliente() {
+        return tipoCliente;
+    }
+
+    public void setTipoCliente(String tipoCliente) {
+        this.tipoCliente = tipoCliente;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getTipoSolicitud() {
+        return tipoSolicitud;
+    }
+
+    public void setTipoSolicitud(String tipoSolicitud) {
+        this.tipoSolicitud = tipoSolicitud;
+    }
+
+    public boolean isMantenimiento() {
+        return mantenimiento;
+    }
+
+    public void setMantenimiento(boolean mantenimiento) {
+        this.mantenimiento = mantenimiento;
+    }
+
+    public boolean isInstalacion() {
+        return instalacion;
+    }
+
+    public void setInstalacion(boolean instalacion) {
+        this.instalacion = instalacion;
+    }
+
+    public String getFotoPath() {
+        return fotoPath;
+    }
+
+    public void setFotoPath(String fotoPath) {
+        this.fotoPath = fotoPath;
     }
 }
